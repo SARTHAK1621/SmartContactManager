5cfd20b2adc19fb3ce9680b670a9fb99934f7101
SARTHAK1621 <sarthaktripathy1621@gmail.com>
2024-05-01 21:24:13 +0530

    database setup is done

---
+++ SmartContachManager/src/main/java/com/example/SCM/SmartContachManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContachManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContachManagerApplication.class, args);
+	}
+
+}
---
+++ SmartContachManager/src/test/java/com/example/SCM/SmartContachManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContachManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
