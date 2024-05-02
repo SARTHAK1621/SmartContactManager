d9ca489ac674f12cc6e817e32dababfd6d7799d3
SARTHAK1621 <sarthaktripathy1621@gmail.com>
2024-05-02 12:04:43 +0530

    day 2

--- SmartContachManager/src/main/java/com/example/SCM/controller/HomeController.java
+++ SmartContachManager/src/main/java/com/example/SCM/controller/HomeController.java
@@ -1,15 +1,23 @@
 package com.example.SCM.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.example.SCM.DAO.UserRepo;
-import com.example.SCM.model.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	@RequestMapping("/")
+	public String home(Model model)
+	{
+		model.addAttribute("title","Home-Smart Contact Manager");
+		return "home";
+	}
 	
+	@RequestMapping("/about")
+	public String about(Model model)
+	{
+		model.addAttribute("title","About-Smart Contact Manager");
+		return "about";
+	}
 
 }
