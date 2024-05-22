b20c22af83dfb8f0bbcbd6c647715b7b017b8a4c
SARTHAK1621 <sarthaktripathy1621@gmail.com>
2024-05-22 13:07:41 +0530

    password change remains

---
+++ SmartContachManager/src/main/java/com/example/SCM/controller/ForgotController.java
@@ -0,0 +1,30 @@
+package com.example.SCM.controller;
+
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ForgotController {
+	Random random=new Random(1000);
+	//emailid form open handler
+	@RequestMapping("/forgot")
+	public String openEmailForm()
+	{
+		return "forgot_email_form";
+	}
+	@PostMapping("/send-OTP")
+	public String sendOTP(@RequestParam("email") String email)
+	{
+		System.out.println(email);
+		// generating otp
+		
+		int otp=random.nextInt(9999);
+		System.out.println(otp);
+		return "verify_OTP";
+	}
+
+}
---
+++ SmartContachManager/src/main/java/com/example/SCM/service/EmailService.java
@@ -0,0 +1,5 @@
+package com.example.SCM.service;
+
+public class EmailService {
+
+}
